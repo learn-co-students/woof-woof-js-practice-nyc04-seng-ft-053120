c05af46ef5fe2b3fb22709ee023cb2c17070e9d6
garcikg0 <kg.gar91@gmail.com>
2020-08-12 13:37:41 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-const dogBarDiv = document.querySelector("#dog-bar")
-const dogInfoDiv = document.querySelector("#dog-info")
+let dogBarDiv = document.querySelector("div#dog-bar")
+let dogInfoDiv = document.querySelector("#dog-info")
+let bonusButton = document.querySelector("button#good-dog-filter") 
 
 fetch('http://localhost:3000/pups')
     .then(res => res.json())
@@ -10,58 +11,44 @@ fetch('http://localhost:3000/pups')
     })
 
 let turnPuptoSpan = (pupObj) => {
-    let pupSpan = document.createElement("span")
-    pupSpan.innerText = pupObj.name
-    dogBarDiv.append(pupSpan)
+    let outerDogSpan = document.createElement("span")
+        outerDogSpan.innerText = pupObj.name
+    
+    dogBarDiv.append(outerDogSpan)
 
-    pupSpan.addEventListener("click", (evt) => {
-        dogInfoDiv.innerHTML = ""
-        renderPup(pupObj)
+    outerDogSpan.addEventListener("click", (evt) => {
+       renderPup(pupObj)
     })
 }
 
 let renderPup = (singlePup) => {
     // console.log("This is the pup we want to render, singlePup")
     let pupImg = document.createElement("img")
+        pupImg.src = singlePup.image
     let pupName = document.createElement("h2")
+        pupName.innerText = singlePup.name
     let pupButton = document.createElement("button")
-    pupImg.src = singlePup.image
-    pupName.innerText = singlePup.name
-    
-    singlePup.isGoodDog ? pupButton.innerText = 'Good Dog!' : pupButton.innerText = 'Bad Dog!'
-
-    // if (singlePup.isGoodDog) {
-       //  pupButton.innerText = "Good Dog!"
-    // } else { pupButton.innerText = "Bad Dog!"}
+        pupButton.innerText = singlePup.isGoodDog ? "Good Dog!" : "Bad Dog!"
 
     dogInfoDiv.append(pupImg, pupName, pupButton)
 
     pupButton.addEventListener("click", (evt) => {
-        
-        singlePup.isGoodDog ? singlePup.isGoodDog = false : singlePup.isGoodDog = true
-        
-        // if (singlePup.isGoodDog) {
-           // pupButton = false
-        // } else { singlePup.isGoodDog = true}
-
-    fetch(`http://localhost:3000/pups/${singlePup.id}`, {
-        method: "PATCH",
-        headers: { 
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            isGoodDog: singlePup.isGoodDog
+        fetch(`http://localhost:3000/pups/${singlePup.id}`, {
+            method: "PATCH",
+            headers: { 
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                isGoodDog: !dogObj.isGoodDog
+            })
         })
-    })
 
     .then(res => res.json())
     .then((updatedPup) => {
-        
-        updatedPup.isGoodDog ? pupButton.innerText = 'Good Dog!' : pupButton.innerText = 'Bad Dog!'
-        
-        // if (updatedButton.isGoodDog) {
-           // pupButton.innerText = "Good Dog!"
-        // } else {pupButton.innerText = "Bad Dog!"}
+        // UPDATING THE OBJECT IN MEMORY
+        singlePup.isGoodDog = updatedPup.isGoodDog
+        // UPDATING THE DOM
+        pupButton.innerText = updatedPup.isGoodDog? "Good Boy!" : "Bad Dog!"
     })
 })
 }
